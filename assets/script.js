06cd3e215f555d827fe0500d279bff68f26432a1
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-28 14:48:10 -0500

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+console.log('it works')
+
+var openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+ key
+var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q'+city+'&limit=5&appid='+ key
+var key = '8e63ee673b69ef9b660b0e01ef8bfc2a'
+
+
+
+function getLocation(city){
+
+}
+
+function getWeather(lat,lon){
+
+}

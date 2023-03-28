dd78117cb4b3b617d583f40ffd3b7d1ab187b9ed
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-28 17:00:43 -0500

    api working

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,51 @@
 console.log('it works')
 
-var openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+ key
-var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q'+city+'&limit=5&appid='+ key
-var key = '8e63ee673b69ef9b660b0e01ef8bfc2a'
+// var openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+ key
+//var geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q'+city+'&limit=5&appid='+ key
+ var key = '9f2cdd9e0dd31d4e4d4ed780e91b0d27'
 
+ var search = $('#search')
+ var textarea = $('#textarea')
 
-
-function getLocation(city){
-
+ function getLocation(city){
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+ key).then(function(response){
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        var lat = data[0].lat
+        var lon = data[0].lon
+        getWeather(lat, lon)
+        getCurrentWeather(lat,lon)
+    })
 }
 
 function getWeather(lat,lon){
-
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+ key).then(function(response){
+        return response.json()}
+    ).then(function(data){
+        console.log(data)
+    })
 }
+
+function getCurrentWeather(lat, lon){
+fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+key).then(function(response){
+return response.json();
+}).then(function(data){
+console.log(data)
+})
+}
+
+function addPreviousSearch(location){
+    console.log('hit 2')
+var liEl = $('<li>')
+liEL.text(location)
+liEL.append(search)  
+}
+
+search.on('click', function(){
+    console.log('hit')
+    searchLocation = textarea.val().trim();
+
+    getLocation(searchLocation);
+    addPreviousSearch(searchLocation)
+})
